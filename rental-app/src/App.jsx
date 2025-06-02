6e7b06cf89fe3d4f05e1f119b8e221a6c1b419dd
Mamtasharma8595 <129156980+Mamtasharma8595@users.noreply.github.com>
2025-06-02 12:39:43 +0100

    Update App.jsx

--- rental-app/src/App.jsx
+++ rental-app/src/App.jsx
@@ -110,6 +110,7 @@ function App() {
                     </ProtectedRoute>
                   }
                 />
+                <Route path="*" element={<Navigate to="/" replace />} />
               </Routes>
             </Layout>
           }
